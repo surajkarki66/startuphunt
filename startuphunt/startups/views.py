4a01903d62b4b626fe3df32304d05f51849e5bc7
surajkarki66 <suraj.karki500@gmail.com>
2019-07-10 21:27:55 +0545

    first commit

---
+++ startuphunt/startups/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
+
+def home(request):
+    products = Product.objects
+    return render(request, 'startups/home.html',{'products':products})
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'startups/create.html',{'error':'All fields are required.'})
+    else:
+        return render(request, 'startups/create.html')
